3bdc8c88fc719adde884cd66473fead8e0c3dd32
Taewoo Kim <twk0128@naver.com>
2022-06-28 00:39:28 +0900

    Custom Combine Publisher For Firebase Auth State

--- Increment/IncrementApp.swift
+++ Increment/IncrementApp.swift
@@ -12,9 +12,21 @@ import Firebase
 struct IncrementApp: App {
     
     @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    @StateObject private var appState = AppState()
+    
     var body: some Scene {
         WindowGroup {
-            LandingView()
+            if appState.isLoggedIn{
+                TabView{
+                    Text("Log")
+                        .tabItem {
+                            Image(systemName: "book")
+                        }
+                }.accentColor(.primary)
+                
+            }else{
+                LandingView()
+            }
         }
     }
 }
@@ -27,5 +39,20 @@ class AppDelegate: NSObject, UIApplicationDelegate {
     -> Bool {
         FirebaseApp.configure()
         return true
+    }
+}
+
+class AppState: ObservableObject {
+    @Published private(set) var isLoggedIn = false
+    
+    private let userService: UserServiceProtocol
+    
+    init(userService: UserServiceProtocol = UserService()) {
+        self.userService = userService
+        try? Auth.auth().signOut()
+        userService
+            .observeAuthChanges()
+            .map{$0 != nil}
+            .assign(to: &$isLoggedIn)
     }
 }
